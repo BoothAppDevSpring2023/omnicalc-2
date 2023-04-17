1f23a8da8aee7541b07a8c17cb81777b02bff838
Mattia Lanzi <mlanzi@chicagobooth.edu>
2023-04-17 15:33:03 +0000

    v1

---
+++ app/controllers/math_controller.rb
@@ -0,0 +1,46 @@
+class MathController < ApplicationController
+
+  def blank_add_form
+  render({ :template => "math_templates/addition_form.html.erb"})
+  end
+
+  def add
+  @first_number = params.fetch("first_number").to_f
+  @second_number = params.fetch("second_number").to_f
+  @sum = @first_number + @second_number
+  render({ :template => "math_templates/addition_results.html.erb"})
+  end
+
+  def blank_subtract_form
+    render({ :template => "math_templates/subtraction_form.html.erb"})
+    end
+
+    def subtract
+    @num1 = params.fetch("num1").to_f
+    @num2 = params.fetch("num2").to_f
+    @dif = @num2 - @num1
+    render({ :template => "math_templates/subtraction_results.html.erb"})
+    end
+
+    def blank_multiply_form
+      render({ :template => "math_templates/multiplication_form.html.erb"})
+      end
+
+      def multiply
+      @num1 = params.fetch("num1").to_f
+      @num2 = params.fetch("num2").to_f
+      @multiply = @num2 * @num1
+      render({ :template => "math_templates/multiplication_results.html.erb"})
+      end
+
+      def blank_division_form
+        render({ :template => "math_templates/division_form.html.erb"})
+        end
+
+        def divide
+        @num1 = params.fetch("num1").to_f
+        @num2 = params.fetch("num2").to_f
+        @divide = @num1 / @num2
+        render({ :template => "math_templates/division_results.html.erb"})
+        end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,23 @@
 Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get("/", { :controller => "math", :action => "addition_home" })
+  
+  get("/add", { :controller => "math", :action => "blank_add_form" })
+
+  get("/subtract", { :controller => "math", :action => "blank_subtract_form" })
+
+  get("/multiply", { :controller => "math", :action => "blank_multiply_form" })
+
+  get("/divide", { :controller => "math", :action => "blank_division_form" })
+
+  get("/wizard_add", { :controller => "math", :action => "add" })
+
+  get("/wizard_subtract", { :controller => "math", :action => "subtract" })
+
+  get("/wizard_multiply", { :controller => "math", :action => "multiply" })
+
+  get("/wizard_divide", { :controller => "math", :action => "divide" })
+
 end
